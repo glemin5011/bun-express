6b82c97c7067e4a19c541b7ac42d866e844696e1
Matej Palenik <matej.palenik@ibm.com>
2025-02-10 17:09:10 +0100

    minor change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process?.env?.SERVER_PORT ?? 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Worlds!");
 });
 
 app.listen(port, () => {
